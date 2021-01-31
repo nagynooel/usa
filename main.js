235a082e06611c687a5c28ee73526c372d657504
Nagy Noel <nagynooel@gmail.com>
2021-01-31 20:48:15 +0100

    Responzív navigációs menü hozzáadva

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("respnav").style.width = "100%";
+    document.getElementById("flag").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("respnav").style.width = "0";
+    document.getElementById("flag").style.display = "block";
+}
