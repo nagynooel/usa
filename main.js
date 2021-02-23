ea7edd5e3c05b51d244229a339de42742be8fe74
Nagy Noel <nagynooel@gmail.com>
2021-02-23 14:36:36 +0100

    BackToTop hozzáadva

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ function closeNav() {
     document.getElementById("respnav").style.width = "0";
     document.getElementById("flag").style.display = "block";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("btt").style.marginRight = "0px";
+    } else {
+        document.getElementById("btt").style.marginRight = "-100px";
+    }
+}
